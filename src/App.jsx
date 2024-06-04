b65c400be041d86b5141ab689d7b7cbea06ae2e2
Maria Shalaginova <pro_m_@mail.ru>
2024-06-04 17:36:38 +0300

    feat добавлен роутинг главной страницы

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import {useState} from 'react'
 import './App.css'
 import HomePage from "./components/HomePage/HomePage.jsx";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
     return (
         <>
-            <HomePage/>
+            <Routes>
+                <Route path="/" element={<HomePage/>} />
+                {/* <Route path= "/auth/login" element ={ <EnterForm />} /> */}
+            </Routes>  
         </>
     )
 }
