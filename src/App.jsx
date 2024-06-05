2321987d1bad4cb1bf1a5dcf7b7e4a4135f09002
Polina Mihaleva <mihalevapolinaros@gmail.com>
2024-06-05 10:34:43 +0300

    feat добавлен календарь для соискателя

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import HomePage from "./components/HomePage/HomePage.jsx";
 import EnterForm from "./components/Forms/EnterForm/EnterForm.jsx";
 import { Route, Routes } from 'react-router-dom';
+import Calendar from "./components/Calendar/Calendar.jsx";
 
 function App() {
 
     return (
         <>
             <Routes>
-                <Route path="/" element={<HomePage/>} />
+                {/*<Route path="/" element={<HomePage/>} />*/}
                 <Route path= "/auth/login" element ={ <EnterForm/>} />
-            </Routes>  
+                <Route path="/" element={<Calendar/>} />
+            </Routes>
         </>
     )
 }
