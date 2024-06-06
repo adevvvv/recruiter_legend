9394147b6b16aa03548c9a2e80f0f37a4a37ce5b
Polina Mihaleva <mihalevapolinaros@gmail.com>
2024-06-06 12:13:26 +0300

    feat создан блок основной информации о соискателе

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,18 +2,35 @@ import Header from "../Header/Header.jsx";
 import AppName from "./AppName.jsx";
 import bottomImage from '../../assets/image/bottomImage.png';
 import styles from './HomePage.module.scss';
+import {useState} from "react";
+import ProfileApplicant from "../ProfileApplicant/ProfileApplicant.jsx";
 
 
 const HomePage = () => {
 
+    // false - не авторизован, applicant, recruiter
+    const [isRole, setIsRole] = useState('applicant');
+
     return (
         <div className={styles['container']}>
-            <Header/>
-            <div style={{alignSelf:"center"}}>
-                <AppName/>
-            </div>
+            <Header isRole={isRole}/>
+            {
+                !isRole ?
+                    <div style={{alignSelf: "center"}}>
+                        <AppName/>
+                        <img alt={'bottomImage'} src={bottomImage}/>
+                    </div>
+                    :
+                    isRole === 'applicant' ?
+                        <ProfileApplicant/>
+                        :
+                        <div>
+                            {/*    Профиль рекрутера*/}
+                        </div>
+            }
 
-            <img alt={'bottomImage'} src={bottomImage}/>
+
+
         </div>
 
     )
