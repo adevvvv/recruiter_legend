9cab2590375f233a37385867170f84a752321a3d
Maria Shalaginova <pro_m_@mail.ru>
2024-06-14 23:04:30 +0300

    feat добавлена верстка страницы рекрутера

--- src/components/ProfileRecruiter/ResponseCard.jsx
+++ src/components/ProfileRecruiter/ResponseCard.jsx
@@ -1,146 +1,97 @@
+/* eslint-disable react/prop-types */
 import styles from './ProfileRecruiter.module.scss';
-import profile from '../../assets/image/profile.svg';
-import chep from '../../assets/image/chep.png';
 import vk from '../../assets/image/vk.png';
 import whatsapp from '../../assets/image/whatsapp.png';
 import telegram from '../../assets/image/telegram.png';
+import accept from '../../assets/image/accept-icon.svg';
+import decline from '../../assets/image/decline-icon.svg'
 
-import { ButtonWithChain } from '../Common/buttons';
+// import { ButtonWithChain } from '../Common/buttons';
 
-const ResponseCard = () => {
-  //   const [data, setFormData] = useState({
-  //     name: 'Имя пользователя',
-  //     bird: 'Дата рождения',
-  //     city: 'Город',
-  //     phone: '+79999999999',
-  //     email: 'email',
-  //   });
-
-  const data = [
-    {
-      firstName: 'Наталья',
-      desiredPosition: 'UX/UI дизайнер',
-      birthDate: '25-02-2000',
-      city: 'Москва',
-      phoneNumber: '+79999998899',
-      email: 'аdsfd@mail.ru',
-      img: {profile},
-    },
-    {
-      firstName: 'Андрей',
-      desiredPosition: 'frontend',
-      birthDate: '25-05-2000',
-      city: 'Казань',
-      phoneNumber: '+79977998899',
-      email: 'аdvsfd@mail.ru',
-      img: '../assets/image/profile2.svg',
-    },
-    {
-      firstName: 'Ольга',
-      desiredPosition: 'DevOps',
-      birthDate: '05-02-1983',
-      city: 'Санкт-Петербург',
-      phoneNumber: '+79999998899',
-      email: 'аdssss@mail.ru',
-      img: '../assets/image/profile3.svg',
-    },
-    {
-      firstName: 'Игорь',
-      desiredPosition: 'Project manager',
-      birthDate: '27-02-2001',
-      city: 'Москва',
-      phoneNumber: '+79669998899',
-      email: 'аdsfd@mail.ru',
-      img: '../assets/image/profile.svg',
-    },
-    {
-      firstName: 'Артем',
-      desiredPosition: 'Backend',
-      birthDate: '25-09-2000',
-      city: 'Москва',
-      phoneNumber: '+79999668899',
-      email: 'kkdsfd@mail.ru',
-      img: '../assets/image/profile2.svg',
-    },
-    {
-      firstName: 'Дмитрий',
-      desiredPosition: 'Fullstack',
-      birthDate: '25-05-2000',
-      city: 'Москва',
-      phoneNumber: '+79999900899',
-      email: 'nnnfd@mail.ru',
-      img: '../assets/image/profile3.svg',
-    },
-  ];
+const ResponseCard = ({user}) => {
   //   const onHandleChange = (e) => {
   //     const { name, value } = e.target;
-  //     // setFormData((data) => ({ ...data, [name]: value }));
+  //     // setFormuser((user) => ({ ...user, [name]: value }));
   //   };
 
   const onClickResume = () => {};
 
   return (
     <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifySelf: 'center',
-      }}
-    >
-      <div className={styles['containerCard']}>
-        <div>
-          <img src={data.img} alt="profile" />
-        </div>
-        <div className={styles['blockForm']}>
-            <div style={{ display: 'flex', gap: '20px' }}>
-                <div className={styles['card-info__text']}>{data.firstName}</div>
-                <div className={styles['card-info__text']}>{data.desiredPosition}</div>
+      className={styles['responseCard']}
+      >
+        <div className={styles['containerCard']}>
+
+            <div  style={{
+             display: 'flex', justifyContent: 'space-around'
+             }}>
+                <div>
+                    <img src={user.img} alt="profile" />
+                </div>
+
+                <div className={styles['blockForm']}>
+                    <div style={{ display: 'flex', gap: '20px' }}>
+                        <div className={styles['card-info__text']}><p>{user.firstName}</p></div>
+                        <div className={styles['card-info__text']}>{user.desiredPosition}</div>
+                    </div>
+                <div style={{ display: 'flex', gap: '20px' }}>
+                    <div className={styles['card-info__text']}>{user.birthDate}</div>
+
+                    <div className={styles['card-info__text']}>{user.city}</div>
+                </div>
+
+                <div style={{ display: 'flex', gap: '20px' }}>
+                    <div className={styles['card-info__text']}>{user.phoneNumber}</div>
+
+                    <div className={styles['card-info__text']}>{user.email}</div>
+                </div>
+
+                <div className={styles['blockNetwork']}>
+                    <div onClick={onClickResume}>
+                    <button>Резюме</button>
+                    </div>
+
+                    <a href="https://vk.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={vk} alt="vk" />
+                    </a>
+
+                    <a
+                    href="https://web.whatsapp.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <img src={whatsapp} alt="whatsapp" />
+                    </a>
+
+                    <a
+                    href="https://web.telegram.org/a/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                    <img src={telegram} alt="telegram" />
+                    </a>
+                </div>
+
+               </div>
             </div>
-          <div style={{ display: 'flex', gap: '20px' }}>
-            <div className={styles['card-info__text']}>{data.birthDate}</div>
 
-            <div className={styles['card-info__text']}>{data.city}</div>
-          </div>
-
-          <div style={{ display: 'flex', gap: '20px' }}>
-            <div className={styles['card-info__text']}>{data.phoneNumber}</div>
-
-            <div className={styles['card-info__text']}>{data.email}</div>
-          </div>
-
-          <div className={styles['blockNetwork']}>
-            <div onClick={onClickResume}>
-              <ButtonWithChain
-                text={'Резюме'}
-                color={'#FFF'}
-                background={'#6C7FBD'}
-              />
+            <div className={styles['blockIcons']} >
+                <a
+                href="https://web.whatsapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                <img src={accept} alt="accept" />
+                </a>
+                <a
+                href="https://web.whatsapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                <img src={decline} alt="decline" />
+                </a>
             </div>
-
-            <a href="https://vk.com/" target="_blank" rel="noopener noreferrer">
-              <img src={vk} alt="vk" />
-            </a>
-
-            <a
-              href="https://web.whatsapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={whatsapp} alt="whatsapp" />
-            </a>
-
-            <a
-              href="https://web.telegram.org/a/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={telegram} alt="telegram" />
-            </a>
-          </div>
         </div>
-      </div>
     </div>
   );
 };
