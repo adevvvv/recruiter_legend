1bfcbf44be192aac66ef784530350e18b90b95be
Maria Shalaginova <pro_m_@mail.ru>
2024-06-16 01:52:50 +0300

    refactor logout

--- src/components/SettingApplicant/SettingApplicant.jsx
+++ src/components/SettingApplicant/SettingApplicant.jsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
 import styles from './SettingApplicant.module.scss';
+import { Context } from '../../main';
 
 const SettingApplicant = ({ setIsRole }) => {
+
+  const { store } = useContext(Context);
+
   const menu = [
     {
       title: 'Чаты и отклики',
@@ -23,6 +28,7 @@ const SettingApplicant = ({ setIsRole }) => {
   const onNavigate = (navigate) => {
     if (navigate === 'exit') {
       setIsRole(false);
+      store.logout();
     }
   };
 
