ae9dc336cbda81a00d1e3d392760733111c50622
Maria Shalaginova <pro_m_@mail.ru>
2024-06-16 21:22:45 +0300

    feat создан спиннер

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,24 @@
+
+import { HashLoader } from "react-spinners";
+
+
+const override = {
+  display: "flex",
+  margin: "auto",
+  borderColor: "red",
+};
+
+const Spinner = () => {
+  return (
+    <div className={override}>
+        <HashLoader
+        color="orange"
+        cssOverride={override}
+        size={50}
+        />
+   </div>
+  );
+  
+};
+
+export default Spinner;
