033a45ebb15458c74f091062aaa186c3a6f7b184
Maria Shalaginova <pro_m_@mail.ru>
2024-06-16 13:58:31 +0300

    feat создан компонент main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,25 +1,12 @@
-// import AppName from './AppName.jsx';
 import bottomImage from '../../assets/image/bottomImage.png';
 import AppName from '../HomePage/AppName.jsx';
 import styles from '../HomePage/HomePage.module.scss';
-// import ProfileApplicant from '../ProfileApplicant/ProfileApplicant.jsx';
-// import ProfileRecruiter from "../ProfileRecruiter/ProfileRecruiter.jsx";
-
-// import ProfileApplicant from '../ProfileApplicant/ProfileApplicant.jsx';
 
 const Main = () => {
 
 
   return (
-    //   <div className={styles['container']}>
-    //   <Header  />
 
-    //     {/* <div style={{ alignSelf: 'center' }}> */}
-    //       <AppName />
-    //       <img alt={'bottomImage'} src={bottomImage} />
-    //     {/* </div> */}
-
-    // </div>
     <div className={styles['container']}>
    
         <div
